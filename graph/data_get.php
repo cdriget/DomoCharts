431397ea0f0be5a3871bea56c0ee8bcfd755e96e
Christophe DRIGET <cdriget@users.noreply.github.com>
2015-09-25 21:04:39 +0200

    Improved debug

--- graph/data_get.php
+++ graph/data_get.php
@@ -2,7 +2,7 @@
 /******************************************************************************/
 /*** File    : data_get.php                                                 ***/
 /*** Author  : Christophe DRIGET                                            ***/
-/*** Version : 5.0                                                          ***/
+/*** Version : 5.01                                                         ***/
 /*** History : February 2014  : Initial release                             ***/
 /***         : May / Sep 2015 : New sensors                                 ***/
 /*** Note    : Get data from database                                       ***/
@@ -29,52 +29,71 @@ set_error_handler('exception_error_handler', E_ALL);
 //*** Main loop
 //***
 try {
+
 	//*** Include necessary files
 	require 'config.inc.php';
+	include 'functions.inc.php';
+
+	//*** Variable initialization
+	$data = array();
 
 	//*** Get data
-	$bdd = new PDO('mysql:host='.$server.';dbname='.$database.';charset=UTF8', $login, $password);
 	if ( isset($_GET['query']) && $_GET['query']=='dataserie' ) {
 		$type   = filter_input(INPUT_GET, 'type',   FILTER_SANITIZE_STRING);
 		$device = filter_input(INPUT_GET, 'device', FILTER_SANITIZE_NUMBER_INT);
 		if (substr($type, -4) == '_day') {
 			if ( $type == 'water_day' )
-				$sql = $bdd->prepare('SELECT UNIX_TIMESTAMP(date)*1000 AS time, sum_value FROM domotique_'.$type.' WHERE device_id = :device ORDER BY time');
+				$query = 'SELECT UNIX_TIMESTAMP(date)*1000 AS time, sum_value FROM domotique_'.$type.' WHERE device_id = :device ORDER BY time';
 			elseif ( $type == 'energy_day' )
-				$sql = $bdd->prepare('SELECT UNIX_TIMESTAMP(date)*1000 AS time, hc_value+hp_value FROM domotique_'.$type.' WHERE device_id = :device ORDER BY time');
+				$query = 'SELECT UNIX_TIMESTAMP(date)*1000 AS time, hc_value+hp_value FROM domotique_'.$type.' WHERE device_id = :device ORDER BY time';
 			elseif ( $type == 'battery_day' )
-				$sql = $bdd->prepare('SELECT UNIX_TIMESTAMP(date)*1000 AS time, value FROM domotique_'.$type.' WHERE device_id = :device ORDER BY time');
+				$query = 'SELECT UNIX_TIMESTAMP(date)*1000 AS time, value FROM domotique_'.$type.' WHERE device_id = :device ORDER BY time';
 			else
-				$sql = $bdd->prepare('SELECT UNIX_TIMESTAMP(date)*1000 AS time, avg_value FROM domotique_'.$type.' WHERE device_id = :device ORDER BY time');
-			$sql->execute(array(
-				'device' => $device)
-			);
+				$query = 'SELECT UNIX_TIMESTAMP(date)*1000 AS time, avg_value FROM domotique_'.$type.' WHERE device_id = :device ORDER BY time';
+			$data['device'] = $device;
 		}
 		elseif (substr($type, -6) == '_month') {
 			if ( $type == 'water_month' )
-				$sql = $bdd->prepare("SELECT UNIX_TIMESTAMP(CONCAT(year,'-',month,'-01 00:00:00'))*1000 AS time, sum_value FROM domotique_".$type.' WHERE device_id = :device ORDER BY time');
+				$query = "SELECT UNIX_TIMESTAMP(CONCAT(year,'-',month,'-01 00:00:00'))*1000 AS time, sum_value FROM domotique_".$type.' WHERE device_id = :device ORDER BY time';
 			else
-				$sql = $bdd->prepare("SELECT UNIX_TIMESTAMP(CONCAT(year,'-',month,'-01 00:00:00'))*1000 AS time, avg_value FROM domotique_".$type.' WHERE device_id = :device ORDER BY time');
-			$sql->execute(array(
-				'device' => $device)
-			);
+				$query = "SELECT UNIX_TIMESTAMP(CONCAT(year,'-',month,'-01 00:00:00'))*1000 AS time, avg_value FROM domotique_".$type.' WHERE device_id = :device ORDER BY time';
+			$data['device'] = $device;
 		}
 		else {
-			$sql = $bdd->prepare('SELECT UNIX_TIMESTAMP(time)*1000 AS time, value FROM domotique_'.$type.' WHERE device_id = :device AND time >= DATE_SUB(curdate(), INTERVAL :interval DAY) ORDER BY time');
-			$sql->execute(array(
-				'device' => $device,
-				'interval' => $interval)
-			);
+			$query = 'SELECT UNIX_TIMESTAMP(time)*1000 AS time, value FROM domotique_'.$type.' WHERE device_id = :device AND time >= DATE_SUB(curdate(), INTERVAL :interval DAY) ORDER BY time';
+			$data['device'] = $device;
+			$data['interval'] = $interval;
 		}
+
+		//*** MySQL connection
+		if ( ! isset($bdd) )
+			$bdd = new PDO('mysql:host='.$server.';dbname='.$database.';charset=UTF8', $login, $password);
+
+		//*** Debug
+		if (DEBUG) {
+			echo 'type = '.$type.'<br/>'.PHP_EOL;
+			echo 'device = '.$device.'<br/>'.PHP_EOL;
+			echo '<pre>'.PHP_EOL;
+			print_r($data);
+			echo '</pre>'.PHP_EOL;
+			echo DebugPDOSQL($query, $data).'<br/>'.PHP_EOL;
+			DEBUG_MYSQL($bdd, $query, $data);
+		}
+
+		//*** Prepare PDO statement
+		$sql = $bdd->prepare($query);
+
+		//*** Execute statement
+		$sql->execute($data);
+
+		//*** Check query success
+		if ( ! $sql ) {
+			$errorInfo = $sql->errorInfo();
+			throw new Exception('SQLSTATE['.$errorInfo[0].'] '.$errorInfo[2], $errorInfo[1]);
+		}
+
+		//*** Retrieve data
 		$datas = $sql->fetchAll(PDO::FETCH_NUM);
-		/*$datas = array();
-		while ( $data = $sql->fetch(PDO::FETCH_ASSOC) ) {
-			$datas['x'][] = $data['time'];
-			$datas['y'][]     = $data['value'];
-			//array_push($timestamp, $data['time']);
-			//array_push($value, $data['value']);
-		}*/
-		//print_r($datas);
 
 		//*** Return data
 		echo $_GET['callback'] . '(' . json_encode($datas, JSON_NUMERIC_CHECK) . ');';
